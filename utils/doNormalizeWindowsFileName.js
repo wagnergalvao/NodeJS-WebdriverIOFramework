7e6c8b4f52516c3d977c5786574b93dd1f15f3a2
Wagner Galvao <galvao.wagner@outlook.com.br>
2020-10-27 17:34:13 -0300

    Refatoração função doNormalizeWindowsFileName.js

--- utils/doNormalizeWindowsFileName.js
+++ utils/doNormalizeWindowsFileName.js
@@ -1,1 +1,4 @@
-exports.doNormalizeWindowsFileName = (text) => text.normalize('NFD').replace(/[^a-zA-Z\u00C0-\u024F\ \^\&\‘\@\{\}\[\]\,\$\=\!\–\#\(\)\%\.\+\~\_]/g, "");
+exports.doNormalizeWindowsFileName = (text) => text
+  .normalize('NFD')
+  .replace(/[^a-zA-Z0-9\u00C0-\u024F\ \^\&\‘\@\{\}\[\]\,\$\=\!\–\#\(\)\%\.\+\~\_]/g, "")
+  .replace(/ /g, '_');
